9abd5938161aa098966706337b0f26c43e2b925f
matthew-compton <compton@bignerdranch.com>
2014-02-04 12:37:13 -0500

    Added date-time and improved layouts

--- src/com/ambergleam/criminalintent/CrimeFragment.java
+++ src/com/ambergleam/criminalintent/CrimeFragment.java
@@ -1,18 +1,28 @@
 package com.ambergleam.criminalintent;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
 
 	private Crime mCrime;
 	private EditText mTitleField;
+	private Button mDateButton;
+	private CheckBox mSolvedCheckBox;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -21,22 +31,19 @@ public class CrimeFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup parent,
-			Bundle savedInstanceState) {
+	public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
 
 		mTitleField = (EditText) v.findViewById(R.id.crime_title);
 		mTitleField.addTextChangedListener(new TextWatcher() {
 
 			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-				mCrime.setmTitle(s.toString());
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				mCrime.setTitle(s.toString());
 			}
 
 			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 				// Intentionally left blank
 			}
 
@@ -44,6 +51,23 @@ public class CrimeFragment extends Fragment {
 			public void afterTextChanged(Editable s) {
 				// Intentionally left blank
 			}
+		});
+
+		mDateButton = (Button) v.findViewById(R.id.crime_date);
+		SimpleDateFormat sdf = new SimpleDateFormat("EEE, MMM dd, yyyy", Locale.US);
+		String date = sdf.format(mCrime.getDate());
+		mDateButton.setText(date);
+		mDateButton.setEnabled(false);
+
+		mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
+		mSolvedCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				// Set the crime's solved property
+				mCrime.setSolved(isChecked);
+			}
+
 		});
 
 		return v;
