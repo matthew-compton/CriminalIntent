2710926f580e85d5df93cac9479dcf875d2ac668
matthew-compton <compton@bignerdranch.com>
2014-02-04 10:29:06 -0500

    Added fragment support

---
+++ src/com/ambergleam/criminalintent/Crime.java
@@ -0,0 +1,27 @@
+package com.ambergleam.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+
+	private UUID mId;
+	private String mTitle;
+	
+	public Crime() {
+		// Generate unique identifier
+		mId = UUID.randomUUID();
+	}
+
+	public UUID getmId() {
+		return mId;
+	}
+
+	public String getmTitle() {
+		return mTitle;
+	}
+
+	public void setmTitle(String mTitle) {
+		this.mTitle = mTitle;
+	}
+	
+}
